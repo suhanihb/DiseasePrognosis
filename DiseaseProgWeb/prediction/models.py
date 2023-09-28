093b495c69c4170a9fa4d61ddb41a3431df3a8d7
Suhani Bhanvadia <suhanihb@gmail.com>
2023-09-27 17:15:48 -0700

    Add checklist

--- DiseaseProgWeb/prediction/models.py
+++ DiseaseProgWeb/prediction/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+import joblib
 
-# Create your models here.
-class TodoItem(models.Model):
-    title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
+def load_trained_model():
+    try:
+        model = joblib.load('../main.pkl')
+        return model
+    except Exception as e:
+        print(f"Error loading the model: {str(e)}")
+        return None
+
+model = load_trained_model()
